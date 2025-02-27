0329f06098f96360ec2c4abb944ddcd4107cb185
zhe-eugene <zhelnitskiy@gmail.com>
2025-02-27 10:32:39 +0200

    init

---
+++ src/js/1-timer.js
@@ -0,0 +1,94 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  dateInput: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('[data-start]'),
+  daysValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secondsValue: document.querySelector('[data-seconds]'),
+};
+
+let userSelectedDate = null;
+let countdownInterval = null;
+
+refs.startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate < new Date()) {
+      iziToast.error({
+        class: '.my-custom-toast',
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        titleColor: '#fff',
+        titleSize: '16px',
+        titleLineHeight: '1.5',
+        messageColor: ' #fff',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
+        backgroundColor: '#ef4040',
+        iconUrl: '../img/octagon.svg',
+        position: 'topRight',
+      });
+      refs.startButton.disabled = true;
+    } else {
+      refs.startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(refs.dateInput, options);
+
+refs.startButton.addEventListener('click', () => {
+  refs.startButton.disabled = true;
+  refs.dateInput.disabled = true;
+
+  countdownInterval = setInterval(() => {
+    const timeLeft = userSelectedDate - new Date();
+
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(0, 0, 0, 0);
+      dateInput.disabled = false;
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    updateTimerDisplay(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  refs.daysValue.textContent = addLeadingZero(days);
+  refs.hoursValue.textContent = addLeadingZero(hours);
+  refs.minutesValue.textContent = addLeadingZero(minutes);
+  refs.secondsValue.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,49 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        class: '.my-custom-toast',
+        title: 'OK',
+        titleColor: '#fff',
+        titleSize: '16px',
+        icon: false,
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topCenter',
+        messageColor: ' #fff',
+        backgroundColor: '#59a10d',
+        iconUrl: '../img/check2-circle.svg',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        class: '.my-custom-toast',
+        title: 'Error',
+        titleColor: '#fff',
+        titleSize: '16px',
+        titleLineHeight: '1.5',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+        messageColor: ' #fff',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
+        backgroundColor: '#ef4040',
+        iconUrl: '../img/octagon.svg',
+      });
+    });
+});
